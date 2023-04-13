d9d7511daf25cb8e6413592bc69a4e3b5316b757
ari <arithmetica2@naver.com>
2023-04-13 09:43:06 +0000

    11

--- RestaurantShare/sendEmail/views.py
+++ RestaurantShare/sendEmail/views.py
@@ -3,4 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 def sendEmail(request):
-    return HttpResponse('sendEmail')
+    
+    
+#     checked_res_list=request.POST.getlist('checks')
+#     inputReceiver=request.POST['inputReceiver']
+#     inputTitle=request.POST['inputTitle']
+#     inputContent=request.POST['inputContent']
+#     return HttpResponse("".join(checked_res_list)+inputReceiver+inputTitle+inputContent)
+
+
+    import smtplib
+
+    gmail_user='22ndistinguishable@gmail.com'
+    gmail_password='zcrrhsxixqtyvhix'
+    
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.ehlo()
+    server.login(gmail_user,gmail_password)
+    server.sendmail('22ndistinguishable@gmail.com',"jsitclub@gmail.com","Hello")
+
+
+        
+    return HttpResponse("sendEmail")
+    
